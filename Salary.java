b9408696dc348eb6a0078b6e714b3eeaf57b5da4
avaladez9 <avaladez9@mail.stmarytx.edu>
2017-01-29 15:56:47 -0600

    Add files via upload

---
+++ Salary.java
@@ -0,0 +1,15 @@
+
+public class Salary {
+	public static void main(String[] args)
+	{
+		String name = "Adam Valadez";
+		int age = 26;
+		double annualPay = 600000.00;
+		
+		System.out.print("My name is " + name);
+		System.out.println(", my age is " + age);
+		System.out.print("and I hope to earn $" + annualPay);
+		System.out.println(" per year.");
+	}
+
+}
